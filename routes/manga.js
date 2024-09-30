c89918a5104f76a3966e2e12d60166c4a0b79647
topDevfullstack <kalvinson961010@gmail.com>
2024-09-30 19:30:21 +0300

    Change chapter download

--- routes/manga.js
+++ routes/manga.js
@@ -7,6 +7,8 @@ const Manga = require('../models/Manga');
 const title = 'Kanojyo to Himitsu to Koimoyou';
 const baseUrl = 'https://api.mangadex.org';
 
+let chaptersAll = [];
+
 // Get all Mangas
 router.get('/', async (req, res) => {
   try {
@@ -20,10 +22,28 @@ router.get('/', async (req, res) => {
       }
     });
 
-    console.log(resp.data.data.map(manga => manga.id));
+    resp.data.data.map(async(manga )=> {
+      // console.log(manga.id);
+      const obj = { mangaId: manga.id, chapters: [] };
+      const respChapters = await axios({
+        method: 'GET',
+        url: `${baseUrl}/manga/${manga.id}/feed`
+      });
+      respChapters.data.data.map(async(chapter) => {
+        // console.log(chapter.id);
+        const respDownloads = await axios({
+          method: 'GET',
+          url: `${baseUrl}/at-home/server/${chapter.id}`
+        });
+        // console.log(respDownloads.data.chapter);
+        obj.chapters.push({ id: chapter.id, title: chapter.attributes.title, downloads: respDownloads.data.chapter });
+      });
+      console.log(obj);
+      chaptersAll.push(obj);
+    });
 
     const mangas = await Manga.find();
-    res.json(mangas);
+    res.json(chaptersAll);
   } catch (err) {
     res.status(500).json({ message: err.message });
   }
--- server.js
+++ server.js
@@ -19,21 +19,7 @@ app.use(express.json());
 mongoose.connect(process.env.MONGODB_URI, {})
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
-
-const title = 'Kanojyo to Himitsu to Koimoyou';
-const baseUrl = 'https://api.mangadex.org';
-
-// const resp = await axios({
-//   method: 'GET',
-//   url: `${baseUrl}/manga`,
-//   params: {
-//     title: title
-//   }
-// });
-
-// console.log(resp.data.data.map(manga => manga.id));
-
-// Example route
+  
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
