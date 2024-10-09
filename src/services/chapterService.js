800350ec87424fa25586aab8847dc36dd1308d9d
topDevfullstack <kalvinson961010@gmail.com>
2024-10-09 10:19:03 -0700

    Change chapterservice/mangaId field to objectId

--- src/services/chapterService.js
+++ src/services/chapterService.js
@@ -4,35 +4,72 @@ const dbService = require('./dbService');
 class ChapterService {
   async insertAllChapters(filter) {
     let res = [];
-    // let count = 0;
-    const mangas = await dbService.getAllMangas(filter);
-    mangas && mangas.map(async (manga, index) => {
-      // const chapterRow = await dbService.getAllChapters({ mangaId: manga.id });
-      // if (count < 10 && chapterRow && !chapterRow.length) {
-        res.push(await this.insertChapters(manga._id, manga.id));
-        // count++;
-      // }
-    });
 
-    // console.log(res);
-    return res;
+    try {
+      const mangas = await dbService.getAllMangas(filter);
+
+      if (mangas && mangas.length) {
+        const chapterPromises = mangas.map(async (manga) => {
+          try {
+            const chapters = await this.insertChapters(manga._id, manga.id);
+            res.push(...chapters); // Assuming insertChapters returns an array of chapters
+          } catch (error) {
+            console.error(`Error inserting chapters for manga ID ${manga.id}:`, error);
+          }
+        });
+
+        // Wait for all insert operations to complete
+        await Promise.all(chapterPromises);
+      }
+    } catch (error) {
+      console.error('Error fetching mangas:', error);
+    }
+
+    return res; // Return the array of inserted chapters
   }
 
   async insertChapters(objId, mangaId) {
     let res = [];
-    const resp = await axios({
-      method: 'GET',
-      url: `${process.env.MANGADEX_URI}/manga/${mangaId}/feed`
-    });
-    resp && resp.data.data.map(async (chapter) => {
-      // console.log(chapter);
-      const chapterData = { id: chapter.id, type: chapter.type, title: chapter.attributes.title, mangaId: objId, createdAt: chapter.attributes.createdAt, updatedAt: chapter.attributes.updatedAt };
-      res.push(chapterData);
-      const chapterRow = await dbService.getAllChapters({ id: chapterData.id });
-      // console.log(chapterRow);
-      if (!chapterRow.length) await dbService.insertChapterCollection(chapterData);
-    });
-    return res;
+
+    try {
+      const resp = await axios({
+        method: 'GET',
+        url: `${process.env.MANGADEX_URI}/manga/${mangaId}/feed`
+      });
+
+      if (resp && resp.data.data) {
+        const chapterPromises = resp.data.data.map(async (chapter) => {
+          const chapterData = {
+            id: chapter.id,
+            type: chapter.type,
+            title: chapter.attributes.title,
+            mangaId: objId,
+            createdAt: chapter.attributes.createdAt,
+            updatedAt: chapter.attributes.updatedAt
+          };
+
+          try {
+            const chapterRow = await dbService.getAllChapters({ id: chapterData.id });
+            if (!chapterRow.length) {
+              const flag = await dbService.insertChapterCollection(chapterData);
+              if (flag) {
+                res.push(chapterData);
+              }
+            }
+          } catch (dbError) {
+            console.error(`Database error for chapter ID ${chapterData.id}:`, dbError);
+          }
+        });
+
+        // Wait for all promises to complete
+        await Promise.all(chapterPromises);
+      }
+    } catch (apiError) {
+      console.error('API call failed:', apiError);
+      // Optionally return an empty array or some other fallback
+    }
+
+    return res; // This will be returned after all processing is completed
   }
 }
 
