b5b53fe4a9195df73af0ccde8ddb1eb40aa2b609
topDevfullstack <kalvinson961010@gmail.com>
2024-10-12 09:11:46 -0700

    Change downModel(add hash field)

--- src/models/downModel.js
+++ src/models/downModel.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 
 const DownSchema = new mongoose.Schema({
   baseUrl: { type: String },
+  hash: { type: String },
   data: [{ type: String }],
   dataSaver: [{ type: String }],
   chapter: { type: mongoose.Schema.Types.ObjectId, ref: 'Chapter', required: true },
